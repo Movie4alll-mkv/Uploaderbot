735acdb2b77c30f740888328de75e3a977af4fca
Movie4alll-mkv <77849768+Movie4alll-mkv@users.noreply.github.com>
2022-12-08 19:04:22 +0500

    Update config.py

--- Uploader/config.py
+++ Uploader/config.py
@@ -77,7 +77,6 @@ class Config(object):
     AUTH_USERS = list({int(x) for x in os.environ.get("AUTH_USERS", "0").split()})
     AUTH_USERS.append(OWNER_ID)
     PORT = "0.0.0.0"
-
     try:
           except FloodWait as e:
         await asyncio.sleep(e.60)  
